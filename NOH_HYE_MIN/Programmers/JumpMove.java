6450592d354cb5e355fdb5c01367feffb49b2854
hyeminnoh <shrmffkfk@naver.com>
2020-05-13 14:20:55 +0900

    [프로그래머스] 점프와 순간이동
    Summer/Winter Coding(~2018) Lv2 문제

---
+++ NOH_HYE_MIN/Programmers/JumpMove.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        while(n>0){
+            if(n%2!=0){
+                n--;
+                ans++;
+            }
+            n/=2;
+        }
+        return ans;
+    }
+}
