5d0b48b64992403a8b4088fa52adb4e5636f1b63
hyeminnoh <shrmffkfk@naver.com>
2020-05-17 17:39:50 +0900

    [프로그래머스] 배달 문제
    Summer/Winter Coding(~2018) Lv3 문제

--- NOH_HYE_MIN/Programmers/Delivery.py
+++ NOH_HYE_MIN/Programmers/Delivery.py
@@ -29,3 +29,36 @@ def solution(N, road, K):
         if w<=K:
             answer+=1
     return answer
+
+
+# Second Solution
+from queue import PriorityQueue
+def dijkstra(road,N):
+    INF = 987654321
+    dist = [INF for i in range(N+1)]
+    road.sort()
+    pq = PriorityQueue()
+    pq.put([0,1])
+    dist[1] = 0
+    while not pq.empty():
+        cost,here = pq.get()
+        if cost > dist[here]: continue
+        for i in range(len(road)):
+            if road[i][0] == here:
+                there,nextCost = road[i][1], road[i][2]+cost
+            if nextCost < dist[there]:
+                dist[there] = nextCost
+                pq.put([nextCost,there])
+            elif road[i][1] == here:
+                there,nextCost = road[i][0], road[i][2]+cost
+            if nextCost < dist[there]:
+                dist[there] = nextCost
+                pq.put([nextCost,there])
+    return dist
+def solution(N, road, K):
+    answer = 0
+    dist = dijkstra(road,N)
+    for d in dist:
+        if d <= K:
+            answer+=1
+    return answer
