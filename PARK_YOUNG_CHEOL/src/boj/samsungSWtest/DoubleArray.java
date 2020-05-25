3c7294da3bb02ed79038a05ae21f8871cd2d9604
dudcheol <parkyc0301@gmail.com>
2020-05-25 22:06:45 +0900

    백준 / 17140 / 이차원 배열과 연산 / 정답

--- PARK_YOUNG_CHEOL/src/boj/samsungSWtest/DoubleArray.java
+++ PARK_YOUNG_CHEOL/src/boj/samsungSWtest/DoubleArray.java
@@ -5,14 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * 정말..오래걸린문제!!!
+ * 배열 범위를 다뤄야 하는 문제는 손으로 그려보던가 해서
+ * 섬세하게 다루도록 한다
+ * 어림잡아 break하거나 continue 해버리는데서 찾기힘든 실수가 발생할 수 있다!!!!
+ *
+ * 또, 배열 사이즈가 계속 달라지는 문제는
+ * temp 배열을 만들어서 덮어씌우는 방법을 사용하도록 하자
+ */
 public class DoubleArray {
     static int r;
     static int c;
     static int k;
     static int[][] ary = new int[100][100];
-    static boolean breakFlag = false;
-    static int rMaxSize = Integer.MIN_VALUE;
-    static int cMaxSize = Integer.MIN_VALUE;
+    static int rMaxSize = 3;
+    static int cMaxSize = 3;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,10 +28,6 @@ public class DoubleArray {
         r = Integer.parseInt(st.nextToken()) - 1;
         c = Integer.parseInt(st.nextToken()) - 1;
         k = Integer.parseInt(st.nextToken());
-
-        for (int[] _ary : ary) {
-            Arrays.fill(_ary, -1);
-        }
 
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
@@ -34,7 +38,7 @@ public class DoubleArray {
 
         int time = 0;
         while (true) {
-            if (ary[r][c] == k || breakFlag) {
+            if (ary[r][c] == k) {
                 break;
             }
             if (time > 100) {
@@ -42,11 +46,23 @@ public class DoubleArray {
                 break;
             }
 
+            String flag = "";
             if (rMaxSize >= cMaxSize) { // R연산
                 operateR();
+                flag+="R";
             } else { //C연산
                 operateC();
+                flag+="C";
             }
+
+//            System.out.println("-------------- " + flag +"연산 ---------");
+//            for (int i=0;i<rMaxSize;i++){
+//                for (int j=0;j<cMaxSize;j++){
+//                    System.out.print(ary[i][j] + " ");
+//                }
+//                System.out.println();
+//            }
+
             time++;
         }
 
@@ -55,13 +71,12 @@ public class DoubleArray {
 
     static void operateR() {
         HashMap<Integer, Integer> map = new HashMap<>();
-        boolean opBreakFlag = false;
-        for (int i = 0; i < ary.length; i++) {
+        ArrayList<ArrayList<Map.Entry<Integer, Integer>>> sortedList = new ArrayList<>();
+        int tempCMaxSize = 0;
+        for (int i = 0; i < rMaxSize; i++) {
             map.clear();
-            for (int j = 0; j < ary.length; j++) {
-                if (ary[i][j] == -1)
-                    break;
-
+            for (int j = 0; j < cMaxSize; j++) {
+                if(ary[i][j] == 0) continue;
                 int target = ary[i][j];
                 if (map.containsKey(target)) {
                     map.put(target, map.get(target) + 1);
@@ -69,35 +84,39 @@ public class DoubleArray {
                     map.put(target, 1);
                 }
             }
-            opBreakFlag = sort(map, i, 'r');
+            ArrayList<Map.Entry<Integer, Integer>> sorted = sort(map, i, 'r');
+            tempCMaxSize = Math.max(tempCMaxSize, sorted.size() * 2);
+            sortedList.add(sorted);
         }
-        breakFlag = opBreakFlag;
+        cMaxSize = tempCMaxSize;
+        drawMap(sortedList, 0);
     }
 
     static void operateC() {
         HashMap<Integer, Integer> map = new HashMap<>();
-        boolean opBreakFlag = false;
-        for (int i = 0; i < ary.length; i++) {
+        ArrayList<ArrayList<Map.Entry<Integer, Integer>>> sortedList = new ArrayList<>();
+        int tempRMaxSize = 0;
+        for (int i = 0; i < cMaxSize; i++) {
             map.clear();
-            for (int j = 0; j < ary.length; j++) {
-                if (ary[j][i] == -1)
-                    break;
-
+            for (int j = 0; j < rMaxSize; j++) {
                 int target = ary[j][i];
+                if (target == 0) continue;
                 if (map.containsKey(target)) {
                     map.put(target, map.get(target) + 1);
                 } else {
                     map.put(target, 1);
                 }
             }
-            opBreakFlag = sort(map, i, 'c');
+            ArrayList<Map.Entry<Integer, Integer>> sorted = sort(map, i, 'c');
+            tempRMaxSize = Math.max(tempRMaxSize, sorted.size() * 2);
+            sortedList.add(sorted);
         }
-        breakFlag = opBreakFlag;
+        rMaxSize = tempRMaxSize;
+        drawMap(sortedList, 1);
     }
 
-    static boolean sort(HashMap<Integer, Integer> map, int i, char type) {
+    static ArrayList<Map.Entry<Integer, Integer>> sort(HashMap<Integer, Integer> map, int i, char type) {
         ArrayList<Map.Entry<Integer, Integer>> listEnt = new ArrayList<>(map.entrySet());
-        boolean opBreakFlag = false;
 
         Collections.sort(listEnt, new Comparator<Map.Entry<Integer, Integer>>() {
             @Override
@@ -107,41 +126,33 @@ public class DoubleArray {
                 } else return o1.getValue().compareTo(o2.getValue());
             }
         });
+        return listEnt;
+    }
 
-        if (type == 'r') {
-            int jIdx = 0;
-            for (Map.Entry<Integer, Integer> s : listEnt) {
-                if (jIdx == 100) {
-                    opBreakFlag = true;
-                    break;
+    static void drawMap(ArrayList<ArrayList<Map.Entry<Integer, Integer>>> sortedList, int type) {
+        int[][] temp = new int[100][100];
+        if (type == 0) { // R 연산
+            for (int i = 0; i < sortedList.size(); i++) {
+                ArrayList<Map.Entry<Integer, Integer>> sorted = sortedList.get(i);
+                int mapEntryIdx = 0;
+                for (int j = 0; j < sorted.size() * 2; j += 2) {
+                    temp[i][j] = sorted.get(mapEntryIdx).getKey();
+                    temp[i][j + 1] = sorted.get(mapEntryIdx).getValue();
+                    mapEntryIdx++;
                 }
-                ary[i][jIdx] = s.getKey();
-                ary[i][jIdx + 1] = s.getValue();
-                jIdx += 2;
             }
-            for (int k = jIdx; k < ary.length; k++) {
-                if (k < 0 || ary[i][k] == -1) break;
-                ary[i][k] = -1;
-            }
-            cMaxSize = Math.max(cMaxSize, Math.max(jIdx - 2, 0));
-        } else {
-            int jIdx = 0;
-            for (Map.Entry<Integer, Integer> s : listEnt) {
-                if (jIdx == 100) {
-                    opBreakFlag = true;
-                    break;
+            ary = temp;
+        } else { // C 연산
+            for (int i = 0; i < sortedList.size(); i++) {
+                ArrayList<Map.Entry<Integer, Integer>> sorted = sortedList.get(i);
+                int mapEntryIdx = 0;
+                for (int j = 0; j < sorted.size() * 2; j += 2) {
+                    temp[j][i] = sorted.get(mapEntryIdx).getKey();
+                    temp[j + 1][i] = sorted.get(mapEntryIdx).getValue();
+                    mapEntryIdx++;
                 }
-                ary[jIdx][i] = s.getKey();
-                ary[jIdx + 1][i] = s.getValue();
-                jIdx += 2;
             }
-            for (int k = jIdx; k < ary.length; k++) {
-                if (k < 0 || ary[k][i] == -1) break;
-                ary[k][i] = -1;
-            }
-            rMaxSize = Math.max(rMaxSize, Math.max(jIdx - 2, 0));
+            ary = temp;
         }
-
-        return opBreakFlag;
     }
 }
