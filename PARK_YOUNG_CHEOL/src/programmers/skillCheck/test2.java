3a49bd54c78b222b1610f3e053aaff1ccc6cc300
dudcheol <parkyc0301@gmail.com>
2020-06-03 13:02:24 +0900

    프로그래머스 / 스킬체크 / 레벨3 / 섬 연결하기 - 정답(답참고)

--- PARK_YOUNG_CHEOL/src/programmers/skillCheck/test2.java
+++ PARK_YOUNG_CHEOL/src/programmers/skillCheck/test2.java
@@ -1,80 +1,57 @@
 package programmers.skillCheck;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
+ * 프로그래머스 - 섬 연결하기
+ *
  * n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때,
  * 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성하세요.
  * 다리를 여러 번 건너더라도, 도달할 수만 있으면 통행 가능하다고 봅니다.
  * 예를 들어 A 섬과 B 섬 사이에 다리가 있고, B 섬과 C 섬 사이에 다리가 있으면 A 섬과 C 섬은 서로 통행 가능합니다.
+ *
+ * 그리디 문제
+ *
+ * 그래프를 그려가며 어렵게 풀 필요 없이
+ * adj 행렬을 이용하여 연결됐을 때마다 방문여부를 확인하고 가장 최소의 비용으로 갈 수 있는 곳을 선택하면 된다
  */
 public class test2 {
-    static int N;
-    static int answer = Integer.MAX_VALUE;
-
-    static class Node {
-        int name;
-        HashMap<Node, Integer> bridge;
-
-        Node(int name) {
-            this.name = name;
-            this.bridge = new HashMap<>();
-        }
-
-        void connectTo(Node node, int cost) {
-            bridge.put(node, cost);
-        }
-    }
 
     static int solution(int n, int[][] costs) {
-        N = n;
+        int answer = 0;
+        // 연결하기
+        int[][] adj = new int[n][n];
 
-        // 노드생성
-        Node[] nodes = new Node[n];
-        for (int i = 0; i < n; i++) {
-            nodes[i] = new Node(i);
-        }
-
-        // 노드연결
         for (int i = 0; i < costs.length; i++) {
             int[] target = costs[i];
-            nodes[target[0]].connectTo(nodes[target[1]], target[2]);
-            nodes[target[1]].connectTo(nodes[target[0]], target[2]);
+            adj[target[0]][target[1]] = adj[target[1]][target[0]] = target[2];
         }
 
-        sol(0, nodes[0], 0, null);
+        boolean[] visit = new boolean[n];
+        visit[0] = true;
+        ArrayList<Integer> connect = new ArrayList<>();
+        connect.add(0);
+
+        while (connect.size() < n) {
+            int minIdx = -1;
+            int min = Integer.MAX_VALUE;
+
+            for (int i : connect) {
+                for (int j = 0; j < n; j++) {
+                    if (adj[i][j] <= 0) continue;
+                    if (visit[j]) continue;
+                    if (adj[i][j] < min) {
+                        min = adj[i][j];
+                        minIdx = j;
+                    }
+                }
+            }
+            connect.add(minIdx);
+            visit[minIdx] = true;
+            answer += min;
+        }
 
         return answer;
-    }
-
-    static void sol(int k, Node curNode, int cost, Node preNode) {
-        if (k == N) { // 전부 방문했으면
-            answer = Math.min(answer, cost);
-            return;
-        }
-
-//        // 현재 노드에서 갈 수 있는 노드 중 가장 짧은 것 찾기
-//        ArrayList<Node> nextNode = new ArrayList<>();
-//        int minCost = Integer.MAX_VALUE;
-//        for (Node node : curNode.bridge.keySet()) {
-//            if (nextNode.size() == 0) {
-//                nextNode.add(node);
-//                minCost = node.bridge.get(curNode);
-//                continue;
-//            }
-//            if (node.bridge.get(curNode) < minCost) {
-//                nextNode.clear();
-//                nextNode.add(node);
-//            } else if (node.bridge.get(curNode) == minCost) {
-//                nextNode.add(node);
-//            }
-//        }
-
-        for (Node next : curNode.bridge.keySet()) {
-            if (preNode != null && next == preNode) continue;
-            sol(k + 1, next, cost + next.bridge.get(curNode), curNode);
-        }
     }
 
     public static void main(String[] args) {
