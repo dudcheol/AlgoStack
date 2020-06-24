9f68daaa17c2fd85c28b3a56ca70b69466e8c537
dudcheol <parkyc0301@gmail.com>
2020-06-24 15:28:34 +0900

    백준 / 14502 / 연구소 / 정답(개선) - 골드5 - 240ms (공간복잡도, 시간복잡도 개선)

--- PARK_YOUNG_CHEOL/src/boj/samsungSWtest/Laboratory.java
+++ PARK_YOUNG_CHEOL/src/boj/samsungSWtest/Laboratory.java
@@ -3,6 +3,7 @@ package boj.samsungSWtest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 
@@ -12,29 +13,19 @@ public class Laboratory {
     static int M;
     static int[][] map;
     static int answer = Integer.MIN_VALUE;
+    static ArrayList<int[]> virusPos;
+    static int[][] tempMap;
     static int[] dx = {-1, 1, 0, 0}; // 상하좌우 이동
     static int[] dy = {0, 0, -1, 1};
 
     static void dfs(int wall, int x) {
         if (wall == 3) {
             // 바이러스가 퍼져나가는 것 시뮬레이션 하기
-            int[][] tempMap = deepCopy(map);
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (map[i][j] == 2) {
-                        spreadVirus(tempMap, i, j);
-                    }
-                }
+//            int[][] tempMap = deepCopy(map);
+            copyMap();
+            for (int[] virus : virusPos) {
+                spreadVirus(virus[0], virus[1]);
             }
-
-//            /* 로그 */
-//            System.out.println("--------------------");
-//            for (int i=0;i<N;i++){
-//                for (int j=0;j<M;j++){
-//                    System.out.print(tempMap[i][j]+" ");
-//                }
-//                System.out.println();
-//            }
 
             // 바이러스 퍼진 후의 안전 영역 크기 구하기
             int safeArea = 0;
@@ -48,35 +39,53 @@ public class Laboratory {
             return;
         }
 
-        for (int i = x; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] != 0) continue; // 빈 칸이 아니면 컨티뉴
-                map[i][j] = 3;
-                dfs(wall + 1, i);
-                map[i][j] = 0;
+//        for (int i = x; i < N; i++) {
+//            for (int j = 0; j < M; j++) { /* 실수 */
+//                if (map[i][j] != 0) continue; // 빈 칸이 아니면 컨티뉴
+//                map[i][j] = 1;
+//                dfs(wall + 1, i);
+//                map[i][j] = 0;
+//            }
+//        }
+        /* 이 방법을 사용하면 배열의 인덱스를 좀 더 효율적으로 접근할 수 있음
+        * 위 주석의 '이중for문'을 사용하면, 지나쳐도될 인덱스까지 검사하게 됨 */
+        for (int i = x; i < N * M; i++) {
+            int _x = i / M;
+            int _y = i % M;
+
+            if (map[_x][_y] == 0) {
+                map[_x][_y] = 1;
+                dfs(wall + 1, i + 1);
+                map[_x][_y] = 0;
             }
         }
     }
 
-    static void spreadVirus(int[][] simulMap, int x, int y) {
+    static void spreadVirus(int x, int y) {
         // 2인 구역을 중심으로 바이러스 전염 시뮬레이션
         for (int dir = 0; dir < 4; dir++) {
             int moveX = x + dx[dir];
             int moveY = y + dy[dir];
             if (moveX < 0 || moveY < 0 || moveX >= N || moveY >= M) continue;
-            if (simulMap[moveX][moveY] == 0) {
-                simulMap[moveX][moveY] = 2;
-                spreadVirus(simulMap, moveX, moveY);
+            if (tempMap[moveX][moveY] == 0) {
+                tempMap[moveX][moveY] = 2;
+                spreadVirus(moveX, moveY);
             }
         }
     }
 
-    static int[][] deepCopy(int[][] origin) {
-        int[][] result = new int[N][M];
-        for (int i = 0; i < origin.length; i++) {
-            result[i] = origin[i].clone();
+//    static int[][] deepCopy(int[][] origin) {
+//        int[][] result = new int[N][M];
+//        for (int i = 0; i < origin.length; i++) {
+//            result[i] = origin[i].clone();
+//        }
+//        return result;
+//    }
+
+    static void copyMap() {
+        for (int i = 0; i < map.length; i++) {
+            tempMap[i] = map[i].clone();
         }
-        return result;
     }
 
     public static void main(String[] args) throws IOException {
@@ -86,11 +95,16 @@ public class Laboratory {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
+        tempMap = new int[N][M];
+        virusPos = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    virusPos.add(new int[]{i, j});
+                }
             }
         }
 
