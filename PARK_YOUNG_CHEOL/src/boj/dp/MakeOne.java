db9c813bc19ffabe2df423032a4cfbc4e14b1406
dudcheol <95_pyc@naver.com>
2020-07-21 23:32:55 +0900

    백준 / 1463 / 1로 만들기 / 정답 - 실수함ㅠ

---
+++ PARK_YOUNG_CHEOL/src/boj/dp/MakeOne.java
@@ -0,0 +1,45 @@
+package boj.dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 실버 3
+ * 정수 X에 사용할 수 있는 연산은 다음과 같이 세 가지 이다.
+ * <p>
+ * X가 3으로 나누어 떨어지면, 3으로 나눈다.
+ * X가 2로 나누어 떨어지면, 2로 나눈다.
+ * 1을 뺀다.
+ * <p>
+ * 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 연산을 사용하는 횟수의 최솟값을 출력하시오.
+ */
+public class MakeOne {
+    static int MAX = 1000001;
+    static int[] cache = new int[1000001];
+
+    static int op(int n) {
+        if (n == 1) {
+            return 0;
+        }
+
+        if (cache[n] != -1) return cache[n];
+
+        int a = MAX, b = MAX, c = MAX;
+        if (n % 3 == 0)
+            a = op(n / 3) + 1;
+        if (n % 2 == 0)
+            b = op(n / 2) + 1;
+        c = op(n - 1) + 1;
+
+        return cache[n] = Math.min(c, Math.min(a, b));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Arrays.fill(cache, -1);
+
+        System.out.println(op(n));
+    }
+}
